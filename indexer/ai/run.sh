d92e6e0c9e1cc33fe99d17635eace00d954edb81
madhuparnab <madhuparnab@google.com>
2020-06-17 19:35:07 +0000

    checkpoint_queue and run.sh removed and added to gitignore

--- indexer/ai/run.sh
+++
@@ -1,3 +0,0 @@
-mvn compile
-export GOOGLE_APPLICATION_CREDENTIALS=/usr/local/google/home/madhuparnab/Downloads/basic-5341ab9846bf.json
-mvn exec:java -Dexec.mainClass=com.google.cloudsearch.CloudSearchAIConnector     -Dexec.args="-Dconfig=property.properties"
