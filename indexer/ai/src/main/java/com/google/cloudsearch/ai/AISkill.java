a39b3ef9a85a73cf3d9b687d46f3263f3a950457
Madhuparna Bhowmik <madhuparnab@google.com>
2020-06-26 23:59:16 +0530

    AISkill Interface, base class, constants and outputMapping classes and exceptions added.

---
+++ indexer/ai/src/main/java/com/google/cloudsearch/ai/AISkill.java
@@ -0,0 +1,37 @@
+package com.google.cloudsearch.ai;
+
+import java.util.List;
+import com.google.cloudsearch.exceptions.InvalidConfigException;
+import com.google.common.collect.Multimap;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+    Interface for all Skills.
+ */
+public interface AISkill {
+
+    public void setAISkillName(String aiSkillName);
+    public String getAISkillName();
+    public void parseAISkillName(String aiSkillName) throws InvalidConfigException;
+
+    public void setOutputMappings(List<OutputMapping> outputMappingList);
+    public List<OutputMapping> getOutputMappings();
+    public void parseOutputMappings(JSONArray outputMapping, JSONObject schema) throws InvalidConfigException;
+
+    public void setInputs(JSONObject input);
+    public JSONObject getInputs();
+    public void parseInputs(JSONObject input) throws InvalidConfigException;
+
+
+    public void setFilter(JSONObject filter);
+    public JSONObject getFilter();
+    public void parseFilter(JSONObject filter) throws InvalidConfigException;
+
+    /**
+     * This function executes the corresponding skill and populate structured data for 1 resource at a time.
+     *
+     * @param contentOrURI      The actual file content or Cloud storage URI
+     */
+    public void executeSkill(String contentOrURI, Multimap<String, Object> structuredData);
+}
