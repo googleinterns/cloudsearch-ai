0735f6671313a6dec0ecc5afd1ef97111cc7c6df
Madhuparna Bhowmik <madhuparnab@google.com>
2020-07-07 18:02:14 +0530

    Return condition changed

--- indexer/ai/src/main/java/com/google/cloudsearch/ai/BaseAISkill.java
+++ indexer/ai/src/main/java/com/google/cloudsearch/ai/BaseAISkill.java
@@ -45,7 +45,7 @@ public abstract class BaseAISkill implements AISkill {
         if(isSchemaInfoStored == false) {
             this.storeSchemaInfo(schema);
         }
-        return !(this.schemaInfo.get(propertyName) == null);
+        return (this.schemaInfo.get(propertyName) != null);
     }
 
     /**
