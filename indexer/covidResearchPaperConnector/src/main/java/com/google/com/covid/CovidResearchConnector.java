07da130d112023b4db0728c692fa7e0feb785b53
Madhuparna Bhowmik <madhuparnab@google.com>
2020-07-15 18:02:01 +0530

    Add Covid Researc Papers Connector

---
+++ indexer/covidResearchPaperConnector/src/main/java/com/google/com/covid/CovidResearchConnector.java
@@ -0,0 +1,16 @@
+package com.google.com.covid;
+
+import com.google.enterprise.cloudsearch.sdk.indexing.IndexingApplication;
+import com.google.enterprise.cloudsearch.sdk.indexing.IndexingConnector;
+import com.google.enterprise.cloudsearch.sdk.indexing.template.FullTraversalConnector;
+import com.google.enterprise.cloudsearch.sdk.indexing.template.Repository;
+
+public class CovidResearchConnector {
+
+    public static void main(String[] args) throws InterruptedException {
+        Repository repository = new LocalRepository();
+        IndexingConnector connector = new FullTraversalConnector(repository);
+        IndexingApplication application = new IndexingApplication.Builder(connector, args).build();
+        application.start();
+    }
+}
